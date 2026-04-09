ac482d881b3949d2aaad780d88c133f936236b2d
OGOUWALE Moumiranti <rantiogouwale24@gmail.com>
2026-04-09 12:06:38 +0100

    Modification du fichier gestion_taches

--- gestion_taches.py
+++ gestion_taches.py
@@ -3,3 +3,8 @@ taches = []
 def ajouter_tache(titre):
     taches.append({"titre": titre, "completee": False})
     print(f"Tâche '{titre}' ajoutée.")
+
+def supprimer_tache(titre):
+    global taches
+    taches = [t for t in taches if t["titre"] != titre]
+    print(f"Tâche '{titre}' supprimée.")
